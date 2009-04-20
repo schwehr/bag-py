ae7c3a9f1f0f36aa7b9f5bda6534a83518151399
schwehr <schwehr@a19cddd1-5311-0410-bb07-9ca93daf0f0b>
2009-04-20 19:32:16 +0000

    starting to process right whale sightings
    git-svn-id: https://cowfish.unh.edu/projects/schwehr/trunk/src/bag-py@11747 a19cddd1-5311-0410-bb07-9ca93daf0f0b

--- bag.py
+++ bag.py
@@ -2,7 +2,7 @@
 
 import h5py
 
-f = h5py.File('sample.bag')
+f = h5py.File('H11302_OLS_OSS/H11302_2m_1.bag')
 
 print f.listobjects()
 print f.listitems()
@@ -10,12 +10,27 @@ print f.listitems()
 bag_root = f['/BAG_root']
 metadata = ''.join(bag_root['metadata'])
 
-print metadata[0:200]
+#print metadata #[0:200]
 
 elevation = bag_root['elevation']
 
-print elevation.shape
+print 'shape:',elevation.shape
 
 data = elevation.value
-print type(data)
-print data
+#print type(data)
+#print data
+
+print 'range:',data.min(), data.max()
+
+#import matplotlib.mlab as mlab
+#import matplotlib.pyplot as plt
+
+o = file('out.xyz','w')
+for y in range(elevation.shape[1]):
+    #for x,z in enumerate(elevation[y]):
+    for x in range(elevation.shape[0]):
+        z = elevation[x,y]
+        if z>=1000000-1:
+            continue
+        #o.write('{x} {y} {z}\n'.format(x=x,y=y,z=z))
+        o.write('%d %d %0.2f\n'% (x,y,z))
