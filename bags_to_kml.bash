124d5ab3ce8f23355bf1769ae745b93923926896
schwehr <schwehr@a19cddd1-5311-0410-bb07-9ca93daf0f0b>
2009-12-15 23:35:09 +0000

    seems to work
    git-svn-id: https://cowfish.unh.edu/projects/schwehr/trunk/src/bag-py@12915 a19cddd1-5311-0410-bb07-9ca93daf0f0b

--- bags_to_kml.bash
+++ bags_to_kml.bash
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash 
+# -e
 
 set -x # Turn on Debugging
 
@@ -12,6 +13,8 @@ set -x # Turn on Debugging
 #rm -f processed/*/*color*.{tif,grd}
 
 #rm -rf processed
+
+PATH=`pwd`:$PATH
 
 for compressed_file in `find ./H10001-H12000/ -name \*.bag.gz`; do 
 #for compressed_file in `find . -name \*.bag.gz | head -1`; do 
@@ -31,7 +34,7 @@ for compressed_file in `find ./H10001-H12000/ -name \*.bag.gz`; do
     mkdir -p processed/$survey
     gzcat $compressed_file > processed/$survey/$src
     pushd processed/$survey
-        rm -f *.png *.tif *.jpg 
+        #rm -f *.png *.tif *.jpg 
         echo Processing patch: $patch
 
         # Get the basic info references
@@ -41,38 +44,49 @@ for compressed_file in `find ./H10001-H12000/ -name \*.bag.gz`; do
 
         ../../bag2kmlbbox.py -o ${patch}-bbox.kml $patch.bag
 
-        ~/local/bin/gdalwarp -ot Float32 -t_srs EPSG:4326  $patch.bag ${patch}-depths-f32.tif
-        echo "FIX: make the color-relief use a ramp that is specific to each patch with gmt and cpt"
-        # -co "nv=0"
-        ~/local/bin/gdaldem color-relief ${patch}-depths-f32.tif ../../color_ramp_fixed.dat  ${patch}-color-relief.tif -alpha -co ALPHA=YES
-        ~/local/bin/gdaldem hillshade ${patch}-depths-f32.tif $patch-hillshade.tif 
-        composite -blend 50 $patch-hillshade.tif ${patch}-color-relief.tif ${patch}-blend.tif # 2&> /dev/null
+        echo "FIX: remove silly small bag size threshold"
+        bag_too_large.py $patch.bag 5000
+        if [ 1 == $? ]; then
+           echo "WARNING: Skipping $patch.bag slow command because file too large"
+           echo "The thumbs will be broken and the web page will not have the required images"
+        else
+            # Okay to generate a tif and do other stuff
+            ~/local/bin/gdalwarp -ot Float32 -t_srs EPSG:4326  $patch.bag ${patch}-depths-f32.tif
+            echo "FIX: make the color-relief use a ramp that is specific to each patch with gmt and cpt"
+            ~/local/bin/gdaldem color-relief ${patch}-depths-f32.tif ../../color_ramp_fixed.dat  ${patch}-color-relief.tif -alpha -co ALPHA=YES
+            ~/local/bin/gdaldem hillshade ${patch}-depths-f32.tif $patch-hillshade.tif 
+            composite -blend 50 $patch-hillshade.tif ${patch}-color-relief.tif ${patch}-blend.tif # 2&> /dev/null
 
-        echo
-        echo "Correcting georeferencing..."
-        listgeo -tfw ${patch}-depths-f32.tif
-        geotifcp -e ${patch}-depths-f32.tfw ${patch}-blend.tif ${patch}.tif 
-     
-        echo "bag_kml_popup.py: ..."
-        ../../bag_kml_popup.py  -b $patch.bag -s $survey -k ../../template.kml \
-            -u http://nrwais1.schwehr.org/~schwehr/bags/H10001-H12000/${survey}/ \
-            -v -o $patch.kml
-        convert -resize 200x200 ${patch}-hist.png ${patch}-hist-thumb-tmp.jpg
-        convert -border 2x2x2x2 -bordercolor black ${patch}-hist-thumb-tmp.jpg ${patch}-hist-thumb.jpg
-        convert ${patch}-hist.png ${patch}-hist.jpg
-        rm ${patch}-hist.png
+            echo
+            echo "Correcting georeferencing..."
+            listgeo -tfw ${patch}-depths-f32.tif
+            geotifcp -e ${patch}-depths-f32.tfw ${patch}-blend.tif ${patch}.tif 
 
-        convert ${patch}-blend.tif ${patch}.jpg
-        convert -resize 200x200 ${patch}-blend.tif ${patch}-thumb-tmp.jpg 
-        convert -border 2x2x2x2 -bordercolor black ${patch}-thumb-tmp.jpg ${patch}-thumb.jpg
+            convert ${patch}-blend.tif ${patch}.jpg
+            convert -resize 200x200 ${patch}-blend.tif ${patch}-thumb-tmp.jpg 
+            convert -border 2x2x2x2 -bordercolor black ${patch}-thumb-tmp.jpg ${patch}-thumb.jpg
 
+            # --zoom=10-12 
+            gdal2tiles.py26 -k -s EPSG:4326 ${patch}.tif
+            mv $patch/doc.kml $patch/${patch}-bathy.kml
+
+            echo "bag_kml_popup.py: ..."
+            ../../bag_kml_popup.py  -b $patch.bag -s $survey -k ../../template.kml \
+                -u http://nrwais1.schwehr.org/~schwehr/bags/H10001-H12000/${survey}/ \
+                -v -o $patch.kml
+            convert -resize 200x200 ${patch}-hist.png ${patch}-hist-thumb-tmp.jpg
+            convert -border 2x2x2x2 -bordercolor black ${patch}-hist-thumb-tmp.jpg ${patch}-hist-thumb.jpg
+            convert ${patch}-hist.png ${patch}-hist.jpg
+
+        fi
+        # Back to non-tif requiring items
+
+
+        # Cleanup
+        #rm -f ${patch}-hist.png
         rm -f ${patch}*-tmp.{jpg,png}
-
-        # --zoom=10-12 
-        gdal2tiles.py26 -k -s EPSG:4326 ${patch}.tif
-        mv $patch/doc.kml $patch/${patch}-bathy.kml
-
-        rm -f *.{tif,tfw,bag} *aux.xml
+        #rm -f *.{tif,tfw} *aux.xml
+        #rm -f *.bag
 
         # --dry-run
         #(cd .. && rsync --exclude-from=../rsync.excludes --verbose --progress --stats -r $survey  nrwais1.schwehr.org:www/bags/H10001-H12000/ )
@@ -80,7 +94,7 @@ for compressed_file in `find ./H10001-H12000/ -name \*.bag.gz`; do
 
     echo
     echo
-    echo sleeping
-    sleep 15
+    echo sleeping to keep the laptop from overheating too badly
+    sleep 1
 done
 
