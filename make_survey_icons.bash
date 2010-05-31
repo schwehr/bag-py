020e74e9879c8b57572d8f0547b219c960479adc
schwehr <schwehr@a19cddd1-5311-0410-bb07-9ca93daf0f0b>
2010-05-31 18:55:29 +0000

    making icons to show the number of bags
    git-svn-id: https://cowfish.unh.edu/projects/schwehr/trunk/src/bag-py@13843 a19cddd1-5311-0410-bb07-9ca93daf0f0b

---
+++ make_survey_icons.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#text_type= ""
+
+in=survey-icon-66x66.png
+
+count=1
+while [[ $count -le 9 ]]; do
+   #echo $count
+    #rm survey-icon-66x66-$count.png
+    convert -fill red -draw "circle 51,51 61,61" -fill white -pointsize 20 -annotate +46+59 $count $in survey-icon-66x66-$count.png
+   let count=$count+1
+done
+
+count=10
+while [[ $count -le 30 ]]; do
+    convert -fill red -draw "circle 51,51 61,61" -fill white -pointsize 20 -annotate +40+59 $count $in survey-icon-66x66-$count.png
+    let count=$count+1
+done 
